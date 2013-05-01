9e1cc93822c632734aaed750d520f8be8ae99de3
Allen Nelson <ithinkican@gmail.com>
2013-05-01 14:51:23 -0500

    first commit

---
+++ create_db.sql
@@ -0,0 +1,80 @@
+CREATE TABLE User (
+	username 		VARCHAR(20) 	NOT NULL,
+	userID 			INTEGER			NOT NULL,
+	fullName 		VARCHAR(100),
+	passwordHash 	VARCHAR(256) 	NOT NULL,
+	email 			VARCHAR(256)	NOT NULL,
+	imageURL 		VARCHAR(200),
+	facebookURL		VARCHAR(200),
+	tagline 		VARCHAR(140),
+	memberSince 	TIMESTAMP		NOT NULL,
+	PRIMARY KEY (userID)
+);
+
+CREATE TABLE Tweet (
+	tweetID INTEGER 				NOT NULL,
+	userID INTEGER	 				NOT NULL,
+	content VARCHAR(140)			NOT NULL,
+	TIMESTAMP			 			NOT NULL,
+	PRIMARY KEY (tweetID),
+	FOREIGN KEY (userID) REFERENCES User(userID)
+);
+
+CREATE TABLE HashTag (
+	tweetID INTEGER 				NOT NULL,
+	content VARCHAR(140) 			NOT NULL,
+	PRIMARY KEY (content),
+	FOREIGN KEY (tweetID) REFERENCES Tweet(tweetID)
+);
+
+CREATE TABLE Follows (
+	follower INTEGER 				NOT NULL,
+	followee INTEGER 				NOT NULL,
+	PRIMARY KEY (follower, followee),
+	FOREIGN KEY (follower) REFERENCES User(userID),
+	FOREIGN KEY (followee) REFERENCES User(userID)
+);
+
+CREATE TABLE Retweets (
+	userID INTEGER 					NOT NULL,
+	tweetID INTEGER 				NOT NULL,
+	TIMESTAMP						NOT NULL,
+	PRIMARY KEY (userID, tweetID),
+	FOREIGN KEY (userID) REFERENCES User(userID),
+	FOREIGN KEY (tweetID) REFERENCES User(tweetID)
+);
+
+CREATE TABLE Mentions (
+	tweetID INTEGER 				NOT NULL,
+	userID INTEGER 					NOT NULL,
+	PRIMARY KEY (tweetID, userID),
+	FOREIGN KEY (tweetID) REFERENCES User(tweetID),
+	FOREIGN KEY (userID) REFERENCES User(userID)
+);
+
+CREATE TABLE Favorites (
+	tweetID INTEGER 				NOT NULL,
+	userID INTEGER 					NOT NULL,
+	PRIMARY KEY (tweetID, userID),
+	FOREIGN KEY (tweetID) REFERENCES User(tweetID),
+	FOREIGN KEY (userID) REFERENCES User(userID)
+);
+
+CREATE TABLE CanSee (
+	tweetID INTEGER				 	NOT NULL,
+	userID INTEGER 					NOT NULL,
+	PRIMARY KEY (tweetID, userID),
+	FOREIGN KEY (tweetID) REFERENCES User(tweetID),
+	FOREIGN KEY (userID) REFERENCES User(userID)
+);
+
+CREATE TABLE Message (
+	messageID INTEGER				NOT NULL,
+	senderID INTEGER 				NOT NULL,
+	receiverID INTEGER	 			NOT NULL,
+	content VARCHAR(140)			NOT NULL,
+	TIMESTAMP			 			NOT NULL,
+	PRIMARY KEY (messageID),
+	FOREIGN KEY (senderID) REFERENCES User(userID)
+	FOREIGN KEY (receiverID) REFERENCES User(userID)
+);
